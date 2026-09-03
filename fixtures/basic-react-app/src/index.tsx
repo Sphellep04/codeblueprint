29db4d183ebb85bdf05ff41ab9984a2c85429b45
Sphellep04 <smlangotz01@gmail.com>
2026-09-03 13:26:03 +0200

    Fix editor tsconfig errors: silence deprecated moduleResolution/baseUrl warnings, stop the fixture's phantom emit-overwrite error, fix a fixture type mismatch

--- fixtures/basic-react-app/src/index.tsx
+++ fixtures/basic-react-app/src/index.tsx
@@ -3,7 +3,7 @@ import ClassComp from "./ClassComp";
 import { UserService, loadUser } from "./services/userService";
 
 function mount() {
-  return [App({}), ClassComp, loadUser(new UserService(), "1")];
+  return [App(), ClassComp, loadUser(new UserService(), "1")];
 }
 
 mount();
